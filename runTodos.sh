f7da5c0a87d2034ed77d4bcde7186dfa46dcca1c
Richard Doyle <rdoyle@scottlogic.com>
2014-09-01 16:58:53 +0100

    Adds Scala-Play launch step to script

--- runTodos.sh
+++ runTodos.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 clear
+
+echo "Launching Scala-Play service..."
+cd backends/scala-play
+start activator "run -Dhttp.port=9002"
+cd ../..
+
+echo "Launching ASP.NET service..."
+
 echo "Launching Todo MVC website..."
-cd web_app
-node server.js
+node web_app/server.js
